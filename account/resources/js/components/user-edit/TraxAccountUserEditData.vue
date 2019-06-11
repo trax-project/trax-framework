e1e40aee68332a4b8889f8e8aee9f2ded964d958
Sébastien Fraysse <sebastien@fraysse.eu>
2019-06-11 16:07:15 +0200

    option to disable profile fields

--- account/resources/js/components/user-edit/TraxAccountUserEditData.vue
+++ account/resources/js/components/user-edit/TraxAccountUserEditData.vue
@@ -4,19 +4,22 @@
         <trax-ui-form :id="id" icons="1" :form="form" :form-extend="formExtend" :action="action" :bus="bus">
 
             <trax-ui-input type="text" icon="person" :placeholder="lang.trax_account.common.username"
-                v-model="form.username" v-bind:error="errors.username" :required="!usernameDisabled" :disabled="usernameDisabled" v-if="withUsername">
+                v-model="form.username" v-bind:error="errors.username" :required="!usernameDisabled" :disabled="usernameDisabled || requiredDisabled" v-if="withUsername">
             </trax-ui-input>
 
             <trax-ui-input type="email" icon="email" :placeholder="lang.trax_account.common.email"
-                v-model="form.email" v-bind:error="errors.email" :required="!emailDisabled" :disabled="emailDisabled">
+                v-model="form.email" v-bind:error="errors.email" :required="!emailDisabled" 
+                :disabled="emailDisabled || requiredDisabled">
             </trax-ui-input>
 
             <trax-ui-input type="text" icon="text_fields" :placeholder="lang.trax_account.common.firstname"
-                v-model="form.firstname" v-bind:error="errors.firstname" required="1">
+                v-model="form.firstname" v-bind:error="errors.firstname" required="1" 
+                :disabled="requiredDisabled">
             </trax-ui-input>
 
             <trax-ui-input type="text" icon="text_fields" :placeholder="lang.trax_account.common.lastname"
-                v-model="form.lastname" v-bind:error="errors.lastname" required="1">
+                v-model="form.lastname" v-bind:error="errors.lastname" required="1"
+                :disabled="requiredDisabled">
             </trax-ui-input>
 
             <trax-ui-select icon="flag" v-model="form.lang" :options="lang_select" v-if="lang_select.length > 1">
@@ -36,7 +39,8 @@
             withUsername: null,
             myProfile: null,
             selfEdit: null,
-            formExtend: null
+            formExtend: null,
+            disableRequiredFields: null
         },
         
         data: function() {
@@ -72,6 +76,10 @@
 
             emailDisabled() {
                 return !this.withUsername && (this.myProfile || this.selfEdit) ? true : null;
+            },
+
+            requiredDisabled() {
+                return this.myProfile && this.disableRequiredFields;
             },
         },
 
