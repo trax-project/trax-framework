53fe6907418a2adf6e74011585a5d0e2c3e01ae3
Sébastien Fraysse <sebastien@fraysse.eu>
2019-06-13 16:13:37 +0200

    UI: responsive table styling

--- ui/resources/js/components/tables/TraxUiResponsiveTable.vue
+++ ui/resources/js/components/tables/TraxUiResponsiveTable.vue
@@ -2,8 +2,8 @@
 
     <div class="trax-responsive-table mb-3">
         <div class="container">
-            <div class="row" v-for="(row, index2) in rows" :key="index2">
-                <div v-for="(cell, index3) in row" :key="index3" :class="colCellClass(index3)" v-html="cell"></div>
+            <div class="row" v-for="(row, rowNum) in rows" :key="rowNum">
+                <div v-for="(cell, colNum) in row" :key="colNum" :class="cellClass(colNum, cell)" v-html="cellContent(cell)"></div>
             </div>
         </div>
     </div>
@@ -20,13 +20,27 @@
 
         methods: {
 
-            colCellClass: function(index) {
+            cellClass: function(colNum, cell) {
                 var cellClass = 'trax-cell col';
-                if (!this.cols || !this.cols[index]) return cellClass;
-                if (this.cols[index].width) cellClass += '-'+this.cols[index].width;
-                if (this.cols[index].class) cellClass += ' '+this.cols[index].class;
+
+                // Add column class and width.
+                if (this.cols && this.cols[colNum]) {
+                    if (this.cols[colNum].width) cellClass += '-'+this.cols[colNum].width;
+                    if (this.cols[colNum].class) cellClass += ' '+this.cols[colNum].class;
+                }
+
+                // Add column class.
+                if (cell.class) {
+                    cellClass += ' ' + cell.class;
+                }
+
                 return cellClass;
+            },
+
+            cellContent: function(cell) {
+                return cell.content ? cell.content : cell;
             }
+
         }
     }
 </script>
